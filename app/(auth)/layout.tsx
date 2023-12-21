7016b830e34e63bf5e73d949412393cbd2c95195
AlejandroSanchez90 <contact@alejandrosanchez.dev>
2023-12-20 21:34:59 -0600

    Authentication
    Authentication using clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function AuthLayout({ children }: Props) {
+  return <div className='flex h-full items-center justify-center'>{children}</div>;
+}
+
+export default AuthLayout;
