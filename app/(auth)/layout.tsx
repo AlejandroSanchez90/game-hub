1af1dbfd47cc4878fc9469ad071f53aa3ff3e2cf
AlejandroSanchez90 <contact@alejandrosanchez.dev>
2023-12-22 10:31:24 -0600

    Dark mode

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Logo } from './_components/logo';
 
 type Props = {
   children: React.ReactNode;
 };
 
 function AuthLayout({ children }: Props) {
-  return <div className='flex h-full items-center justify-center'>{children}</div>;
+  return (
+    <div className='flex flex-col h-full items-center justify-center space-y-6'>
+      <Logo />
+      {children}
+    </div>
+  );
 }
 
 export default AuthLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className='flex flex-col gap-4'>
       <h1>Dashboard</h1>
+      <Button className='w-fit'>Hola</Button>
       <UserButton afterSignOutUrl='/' />
     </div>
   );
