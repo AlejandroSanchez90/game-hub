1ec9944feedd81d18b2c10d8931cec144c814748
AlejandroSanchez90 <contact@alejandrosanchez.dev>
2024-02-04 13:15:32 -0600

    Header

--- components/live-badge.tsx
+++ components/live-badge.tsx
@@ -9,7 +9,7 @@ export function LiveBadge({ className }: Props) {
   return (
     <div
       className={cn(
-        'bg-rose-500 text-center p-0.5 px-1.5 rounded-md uppercase text-[10px] border border-background font-semibold tracking-wide animate-pulse',
+        'bg-rose-500 text-center p-0.5 px-1.5 rounded-md uppercase text-[10px] border border-background font-semibold tracking-wide ',
         className,
       )}
     >
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -9,6 +9,7 @@ import { Video, VideoSkeleton } from './video';
 import { Chat } from './chat';
 import { ChatToggle } from './chat-toggle';
 import { ChatSkeleton } from './chat-message';
+import { Header, HeaderSkeleton } from './header';
 type Props = {
   user: User & { stream: Stream | null };
   stream: Stream;
@@ -39,6 +40,14 @@ export function StreamPlayer({ user, stream, isFollowing }: Props) {
       >
         <div className='space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10'>
           <Video hostName={user.username} hostIdentity={user.id} />
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
         </div>
 
         <div className={cn('col-span-1', collapsed && 'hidden')}>
@@ -62,7 +71,7 @@ export const StreamPlayerSkeleton = () => {
     <div className='grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full'>
       <div className='space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10'>
         <VideoSkeleton />
-        {/* TODO: HEADERR SKELETON */}
+        <HeaderSkeleton />
       </div>
       <div className='col-span-1 bg-background'>
         <ChatSkeleton />
